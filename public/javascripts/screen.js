04075f3a7383930c309ecf3ede2bc92adaf97874
Jeffy <yuanjiefeng@gmail.com>
2014-10-11 12:55:31 +0800

    aaaa

--- public/javascripts/screen.js
+++ public/javascripts/screen.js
@@ -1,12 +1,12 @@
 var href = 'http://10.5.16.17:3000/control.html';
 var socket = io.connect('http://10.5.16.17');
-
 socket.on('news', function () {
     socket.emit('screen');
 });
-socket.on('reply', function (data) {
-    $('.qrcode').qrcode(href + '#' + data.id);
-    console.log(href + '#' + data.id);
+socket.on('usable', function (data) {
+    $('.qrcode')
+        .empty()
+        .qrcode(href + '#' + data.id).show();
 });
 socket.on('connect success', function () {
     $('.qrcode').hide();
